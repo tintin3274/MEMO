6079f1abb38e13eaf933cc3f91052ad6acfeb271
tintin3274 <tintin3274@hotmail.co.th>
2020-11-26 15:46:41 +0700

    change DB version

--- app/src/main/java/th/ku/memo/model/DatabaseHelper.java
+++ app/src/main/java/th/ku/memo/model/DatabaseHelper.java
@@ -7,7 +7,7 @@ import androidx.annotation.Nullable;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "MEMO.db";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
 
     public DatabaseHelper(@Nullable @org.jetbrains.annotations.Nullable Context context) {
